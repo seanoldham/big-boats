7516d4fb66c289968ac11607105300343c1c5229
Sean Oldham <seanosaur@me.com>
2016-09-08 15:01:58 -0500

    no more dead links?

--- lib/flickr.rb
+++ lib/flickr.rb
@@ -4,16 +4,22 @@ def flickr
                           "&tags=tanker,ship" +
                           "&tag_mode=all" +
                           "&format=json" +
-                          "&nojsoncallback=1"
+                          "&nojsoncallback=1" +
+                          "&per_page=500" +
+                          "&extras=url_h"
                           )
   body = JSON.parse(response.body)
-  photo = body["photos"]["photo"][rand(100)]
+  photo = body["photos"]["photo"][rand(500)]
   id = photo["id"]
   owner = photo["owner"]
   secret = photo["secret"]
   server_id = photo["server"]
   farm_id = photo["farm"]
   title = photo["title"]
-  @photo_url = "https://farm#{farm_id}.staticflickr.com/#{server_id}/#{id}_#{secret}_h.jpg"
+  puts photo
+
+  @photo_url = photo["url_h"]
+  # || "https://farm#{farm_id}.staticflickr.com/#{server_id}/#{id}_#{secret}_h.jpg"
   @photo_link = "https://www.flickr.com/photos/#{owner}/#{id}"
+
 end
